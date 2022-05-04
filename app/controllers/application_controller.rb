1da806da6ab5f8e7a77b6a999356ec649bf19256
e601201 <k.arthur1111@gmail.com>
2022-05-05 06:32:12 +0900

    [ADD]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :set_data
+
+
+  def set_data
+    @week = ["mon",
+             "tue",
+             "wed",
+             "thu",
+             "fri",
+             "sat",
+             "sun"]
+    @persons = ["a","b","c"]
+  end
 end
--- app/javascript/title_it.js
+++ app/javascript/title_it.js
@@ -1,31 +1,12 @@
-const timer = document.getElementById('timer');
-const start = document.getElementById("start");
-const stop = document.getElementById("stop");
-let startTime;
-
-//startボタンを押した時にイベントが起動
-start.addEventListener('click', () => {
- startTime = Date.now();
-//  console.log(startTime);
-//  console.log(new Date(startTime))
- countUp();
-});
-
-stop.addEventListener('click',() =>{
-  countStop();
-});
-//coutUp()関数の中身
-function countUp() {
- const d=new Date(Date.now()-startTime);
- const m=String(d.getMinutes()).padStart(2,"0");
- const s=String(d.getSeconds()).padStart(2,"0");
- timer.textContent = `${m}:${s}`;
-
- setTimeout(() => {
-   countUp();
- }, 10);
-}
-
-function countStop() {
-  console.log("stop")
+window.testIt = (person,day) => {
+  const saveCheckbox = document.getElementById(`js_${person}_shift_${day}`)
+  const begin_at = document.getElementById(`js_${person}_begin_at_${day}`);
+  const end_at = document.getElementById(`js_${person}_end_at_${day}`);
+  if (saveCheckbox.checked){
+    begin_at.disabled = true;
+    end_at.disabled = true;
+  }else{
+    begin_at.disabled = false;
+    end_at.disabled = false;
+  }
 }
